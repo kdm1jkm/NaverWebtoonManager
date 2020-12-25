6a6c34d261080f811e2e668a3125e743ccb2cfd4
kdm1jkm <kdm1jkmm@gmail.com>
2020-12-25 13:54:16 +0900

    Add kotlin dependency

--- src/main/java/com/github/kdm1jkm/nwmanager/main/Main.java
+++
@@ -1,7 +0,0 @@
-package com.github.kdm1jkm.nwmanager.main;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ src/main/java/com/github/kdm1jkm/nwmanager/main/Main.kt
@@ -0,0 +1,5 @@
+package com.github.kdm1jkm.nwmanager.main
+
+fun main(args: Array<String>) {
+    println("Hello, World!")
+}
